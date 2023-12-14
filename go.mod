f693c7779dcb3e57d7e189ba4d55cc394439dcff
Htoo Pyae <htoopyae.dev@gmail.com>
2023-12-14 15:18:42 +0700

    done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hpbyte/go_load_balancer
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+const (
+	Attempts int = iota
+	Retry
+)
+
+const RETRY_LIMIT int = 3
+
+// add a backend to the server pool
+func (s *ServerPool) AddBackend(backend *Backend) {
+	s.backends = append(s.backends, backend)
+}
+
+/**
+ * multiple clients will connet to the LB
+ * each requests will need to be passed to the next peer
+ */
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+/**
+ * requests are cycled thru all backends, but dead ones need to be skipped
+ * returns next active peer to take a connection
+ */
+func (s *ServerPool) GetNextPeer() *Backend {
+	// loop entire backends to find out an Alive backend
+	next := s.NextIndex()
+	l := len(s.backends) + next // start from next and move a full cycle
+
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends) // take an index by modding with length
+		// if we have an alive backend, use it and store if it's not the original one
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx)) // mark the current one
+			}
+			return s.backends[idx]
+		}
+	}
+
+	return nil
+}
+
+// set the stutus of a backend
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.backends {
+		if b.URL.String() == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
+}
+
+func (s *ServerPool) HealthCheck() {
+	for _, b := range s.backends {
+		status := "up"
+		alive := isBackendAlive(b.URL)
+		b.SetAlive(alive)
+		if !alive {
+			status = "down"
+		}
+		log.Printf("%s [%s]\n", b.URL, status)
+	}
+}
+
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.RLock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+func GetAttemptsFromContext(r *http.Request) int {
+	if attempts, ok := r.Context().Value(Attempts).(int); ok {
+		return attempts
+	}
+	return 1
+}
+
+func GetRetryFromContext(r *http.Request) int {
+	if retries, ok := r.Context().Value(Retry).(int); ok {
+		return retries
+	}
+	return 1
+}
+
+func isBackendAlive(u *url.URL) bool {
+	timeout := 2 * time.Second
+	conn, err := net.DialTimeout("tcp", u.Host, timeout)
+	if err != nil {
+		log.Println("Site unreachable, error: ", err)
+		return false
+	}
+	_ = conn.Close() // close it, we don't need to maintain this connection
+	return true
+}
+
+// load balances the incoming requests
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := serverPool.GetNextPeer()
+
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
+func healthCheck() {
+	t := time.NewTicker(time.Second * 20)
+	for {
+		select {
+		case <-t.C:
+			log.Println("Starting healthcheck ...")
+			serverPool.HealthCheck()
+			log.Println("HealthCheck completed")
+		}
+	}
+}
+
+var serverPool ServerPool
+
+func main() {
+	var serverList string
+	var port int = 30
+
+	flag.StringVar(&serverList, "backends", "", "Load balanced backends, use commas to separate")
+	flag.IntVar(&port, "port", 3030, "Port to serve")
+	flag.Parse()
+
+	if len(serverList) == 0 {
+		log.Fatal("Please provide one or more backends to load balance")
+	}
+
+	// parse servers
+	tokens := strings.Split(serverList, ",")
+	for _, tok := range tokens {
+		serverUrl, err := url.Parse(tok)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(serverUrl)
+		proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, e error) {
+			log.Printf("[%s] %s\n", serverUrl.Host, e.Error())
+			retries := GetRetryFromContext(request)
+			if retries < RETRY_LIMIT {
+				select {
+				case <-time.After(10 * time.Millisecond):
+					ctx := context.WithValue(request.Context(), Retry, retries+1)
+					proxy.ServeHTTP(writer, request.WithContext(ctx))
+				}
+				return
+			}
+
+			// after 3 retries, mark this backend as down
+			serverPool.MarkBackendStatus(serverUrl, false)
+
+			// if the same request routing for few requests with different backends, increase the counter
+			attempts := GetAttemptsFromContext(request)
+			log.Printf("%s(%s) Attempting retry %d\n", request.RemoteAddr, request.URL.Path, attempts)
+			ctx := context.WithValue(request.Context(), Attempts, attempts+1)
+			lb(writer, request.WithContext(ctx))
+		}
+
+		serverPool.AddBackend(&Backend{
+			URL:          serverUrl,
+			Alive:        true,
+			ReverseProxy: proxy,
+		})
+
+		log.Printf("Configured server: %s\n", serverUrl)
+	}
+
+	// create http server
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(lb),
+	}
+
+	// run the healthcheck in a separate go routine
+	go healthCheck()
+
+	log.Printf("Load balancer started at :%d\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
